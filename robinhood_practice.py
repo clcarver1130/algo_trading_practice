45ec335e8b08623ff262e02c09c8bd547c440e85
CARVER <CAMERON.CARVER@int.asurion.com>
2020-05-28 08:30:49 -0500

    test

--- robinhood_practice.py
+++ robinhood_practice.py
@@ -1,4 +1,10 @@
+# Main libraries
 import pandas as pd
 import schedule
 import time
 from logger import logging
+
+# Robinhood specific libraries and login
+import robin_stocks as r
+from logins import robin_email, robin_password
+login = r.login()
