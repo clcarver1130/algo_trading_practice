42634ac455c1504eee80c32fa95bb04dd44c6be0
CARVER <CAMERON.CARVER@int.asurion.com>
2020-05-31 13:52:14 -0500

    indicators

--- robinhood_practice.py
+++ robinhood_practice.py
@@ -3,8 +3,46 @@ import pandas as pd
 import schedule
 import time
 from logger import logging
+from talib import MACD, ADX, MINUS_DI, PLUS_DI
 
 # Robinhood specific libraries and login
 import robin_stocks as r
 from logins import robin_email, robin_password
-login = r.login()
+login = r.login(robin_email, robin_password)
+
+def scheduler():
+    logging.info('Starting script...')
+    schedule.every(1).hours.do(main)
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
+def main():
+
+    now = str(pd.Timestamp.today())[0:16]
+    logging.info('Calculating metrics for {now}...'.format(now=now))
+
+    df_historical = get_historical_data('ETH', 'hour', '3month')
+    current_indicators = calculate_indicators(df_historical)
+    print(current_indicators)
+
+def get_historical_data(symbol, agg, time_window):
+    df = pd.DataFrame(r.get_crypto_historical(symbol, agg, time_window, '24_7')['data_points'])
+    print(df.info)
+    df['begins_at'] = df['begins_at'].dt.tz_convert('Central')
+    df.set_index('begins_at')
+    return df
+
+def calculate_indicators(df):
+
+    # MACD
+    fast = 12
+    slow = 26
+    signal = 9
+    macd, macdsignal, macdhist = MACD(df['close_price'], fast, slow, signal)
+    macd_current = macd[-1]
+    signal_current = macdsignal[-1]
+    return macd
+    # ADX
+    ADX
+    # DI+/DI-
---
+++ robinhoodstrategy.py
@@ -0,0 +1,7 @@
+import robin_stocks as r
+
+class crypto_strategy(object):
+    """docstring for crypto_strategy."""
+
+    def __init__(self, strategy_name):
+        self.name = strategy_name
