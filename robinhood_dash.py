00901750e4f13862c4f757cae7eb6ae9240e52ce
CARVER <CAMERON.CARVER@int.asurion.com>
2020-06-03 08:39:21 -0500

    page refresh on reload

--- robinhood_dash.py
+++ robinhood_dash.py
@@ -18,47 +18,53 @@ def load_sqlite_data(sql, DB_NAME):
     df = pd.read_sql(sql, conn)
     return df
 
+
+
 ### Build the Web App:
-df = load_sqlite_data(sql, DB_NAME)
 
 app = dash.Dash(__name__)
 
-candle_fig = go.Figure(data=[go.Candlestick(x=df['time_period_start'],
-                open=df['price_open'],
-                high=df['price_high'],
-                low=df['price_low'],
-                close=df['price_close'])])
+def load_layout():
+    df = load_sqlite_data(sql, DB_NAME)
 
-app.layout = html.Div([
-                        # Title
-                        html.H1('Ethereum Trading Bot'),
+    candle_fig = go.Figure(data=[go.Candlestick(x=df['time_period_start'],
+                    open=df['price_open'],
+                    high=df['price_high'],
+                    low=df['price_low'],
+                    close=df['price_close'])])
 
-                        # Candlestick Chart:
-                        dcc.Graph(figure=candle_fig),
+    return html.Div([
+                # Title
+                html.H1('Ethereum Trading Bot'),
 
-                        # MACD Chart:
-                        dcc.Graph(
-                            figure = {
-                                'data': [
-                                    go.Scatter(
-                                    x = pd.to_datetime(df['time_period_start']),
-                                    y = df['macd_current'],
-                                    mode = 'lines+markers'
-                                                ),
-                                    go.Scatter(
-                                    x = pd.to_datetime(df['time_period_start']),
-                                    y = df['macd_signal_current'],
-                                    mode = 'lines+markers'
-                                                )
-                                        ],
-                                 'layout' : go.Layout(
-                                    xaxis={'title': 'Time (Zulu)'},
-                                    yaxis={'title': 'MACD'},
-                                    margin={'l': 50, 'b':50, 't': 10, 'r': 50},
-                                            )
-                                    },
-                                )
-                        ])
+                # Candlestick Chart:
+                dcc.Graph(figure=candle_fig),
+
+                # MACD Chart:
+                dcc.Graph(
+                    figure = {
+                        'data': [
+                            go.Scatter(
+                            x = pd.to_datetime(df['time_period_start']),
+                            y = df['macd_current'],
+                            mode = 'lines+markers'
+                                        ),
+                            go.Scatter(
+                            x = pd.to_datetime(df['time_period_start']),
+                            y = df['macd_signal_current'],
+                            mode = 'lines+markers'
+                                        )
+                                ],
+                         'layout' : go.Layout(
+                            xaxis={'title': 'Time (Zulu)'},
+                            yaxis={'title': 'MACD'},
+                            margin={'l': 50, 'b':50, 't': 10, 'r': 50},
+                                    )
+                            },
+                        )
+                ])
+
+app.layout = load_layout
 
 if __name__ == '__main__':
     app.run_server(debug=True, host='0.0.0.0')
