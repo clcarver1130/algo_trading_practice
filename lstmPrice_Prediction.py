7186fec70fdfcc4bbd3dd18864e84bfe6bf5cac3
clcarver1130 <carver.cameron@gmail.com>
2020-09-02 15:31:02 -0500

    class testing

---
+++ lstmPrice_Prediction.py
@@ -0,0 +1,58 @@
+import numpy as np
+import pandas as pd
+import datetime
+import matplotlib.pyplot as plt
+import seaborn as sns
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, LSTM, Dropout, Activation
+
+class PricePrediction:
+    
+    def __init__(self):
+        self.raw_data = None
+        self.n_input = None
+        self.n_out = None
+        self
+        
+    def load_dataframe(self,df):
+        self.raw_data = df
+        
+    def split_data(self, training_size=0.8):
+        return self.raw_data[:int(training_size*len(self.raw_data))], self.raw_data[int(training_size*len(self.raw_data)):]
+
+    def make_3d(self, array):
+        return array.reshape(array.shape[0], array.shape[1], 1)
+    
+    def to_supervised(self, df, n_input, n_out, _priceCol=3):
+        
+        data = np.array(df)
+        
+        self.n_input = n_input
+        self.n_out = n_out
+        
+        X, y = list(), list()
+        in_start = 0
+        # step over the entire history one time step at a time
+        for _ in range(len(data)):
+            # define the end of the input sequence
+            in_end = in_start + n_input
+            out_end = in_end + n_out
+            # ensure we have enough data for this instance 
+            if out_end <= len(data):
+                X.append(data[in_start:in_end, :])
+                y.append(data[in_end:out_end, _priceCol])
+            # move along one time step
+            in_start += 1
+        return np.array(X), np.array(y).reshape(-1)
+    
+    def normalize_inputs(self, X, y):
+        
+        x_norm = np.array([x/x[0]-1 for x in X])
+        y_norm = np.array([y/x[0][3]-1 for x,y in zip(X, y)])
+        return x_norm, y_norm
+    
+    denormalize_outputs(self, b)
+        
+        
+
+        
--- robinhoodstrategy.py
+++ robinhoodstrategy.py
@@ -1,7 +1,158 @@
-import robin_stocks as r
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import time
+import datetime
+from coinapi_rest_v1 import CoinAPIv1
+from robin_helperfunctions import round_to_hour
 
-class crypto_strategy(object):
+class cryptoStrategy:
+    
     """docstring for crypto_strategy."""
 
     def __init__(self, strategy_name):
         self.name = strategy_name
+        self.entry_func = None
+        self.exit_func = None
+        
+    def add_entry_conditions(self, entry_func):
+        self.entry_func = entry_func
+        return
+        
+    def add_exit_conditions(self, exit_func):
+        self.exit_func = exit_func
+        return
+        
+    def change_position(self, position_flag, func_args):     
+        if position_flag:
+            return self.exit_func(**func_args)
+        else:
+            return self.entry_func(**func_args)
+
+class Trade:
+    
+    '''An object to hold metadata about a trade'''
+    
+    def __init__(self):
+        self.datetime_buy = None
+        self.datetime_sell = None
+        self.price_buy = None
+        self.price_sell = None
+        self.period_count = 0
+        self.current_price = None
+        self.pct_change = None
+        
+    def log_buy(self, time, price):
+        self.datetime_buy = time
+        self.price_buy = price
+        
+    def log_sell(self, time, price):
+        self.datetime_sell = time
+        self.price_sell = price
+        return {'trade_start':self.datetime_buy, 
+                'trade_end':self.datetime_sell, 
+                'peirods':self.period_count, 
+                'buy_price': self.price_buy, 
+                'sell_price':self.price_sell,
+                'pct_change': (self.price_sell-self.price_buy)/self.price_buy}
+        
+    def log_pass(self, current_price):
+        self.period_count += 1
+        self.current_price = current_price
+        self.pct_change = (self.current_price-self.price_buy)/self.current_price
+        
+        
+    
+        
+class BackcastStrategy:
+    
+    '''docstring for backcasting'''
+    
+    def __init__(self, strategy):
+        self.strategy = strategy
+        self.starting_capital = None
+        self.symbol = None
+        self.start_datetime = None
+        self.end_datetime = None
+        self.data_agg = None
+        self.period_check = None
+        self.periods_needed = None
+        self.trades = dict()
+        
+    def set_parameters(self, starting_capital, coinapi_symbol, start_datetime, end_datetime, data_agg, period_check, periods_needed):
+        self.capital = starting_capital
+        self.symbol = coinapi_symbol
+        self.start_datetime = start_datetime
+        self.end_datetime = end_datetime
+        self.data_agg = data_agg
+        self.period_check = period_check
+        self.periods_needed = periods_needed
+        self.backcast_data = None
+        return 
+    
+    def _get_backcast_data(self, CoinAPI_KEY):
+        
+        coin_api = CoinAPIv1(CoinAPI_KEY)
+        params = {'period_id':self.data_agg, 'time_start':self.start_datetime, 'time_end':self.end_datetime, 'limit':100000}
+        df = pd.DataFrame(coin_api.ohlcv_historical_data(self.symbol, params)).set_index('time_period_start')
+        return df
+    
+    def run_backcast(self, CoinAPI_KEY):
+        
+        print(f'Running backcast between {self.start_datetime} and {self.end_datetime}. Staring with ${self.starting_capital}')
+        
+        position_amount = 0
+        position_flag = False
+        trades_dict = dict()
+        
+        self.backcast_data = self._get_backcast_data(CoinAPI_KEY)
+        for i in range(len(self.backcast_data) - self.periods_needed):
+            df_sliced = self.backcast_data[i:self.periods_needed + i]
+            current_price = df_sliced['price_close'][-1]
+            if self.strategy.change_position(position_flag, {'hist_data':df_sliced}):
+                if position_flag:
+                    # Execute sell
+                    self.capital = position_amount * current_price
+                    position_amount = 0
+                    position_flag = False
+                    trades_dict[i] = trade.log_sell(df_sliced.index[-1], current_price)
+                else:
+                    # Execute buy:
+                    position_amount = self.capital/current_price
+                    self.capital = 0
+                    position_flag = True
+                    trade = Trade()
+                    trade.log_buy(df_sliced.index[-1], current_price)
+            else:
+                if position_flag:
+                    trade.log_pass(current_price)
+                pass
+        
+        # Close position at the end of the backcast:
+        if position_flag:
+            self.capital = position_amount * current_price
+            
+        
+        self.trades = pd.DataFrame.from_dict(trades_dict, orient='index').reset_index(drop=True)
+        
+    def plot_trades(self):
+        
+        plt.style.use('seaborn-whitegrid')
+
+        plt.figure(figsize=(30,8))
+
+        plt.plot(self.backcast_data.index, self.backcast_data['price_close'], lw=2)
+        plt.xticks([])
+
+        plt.scatter(self.trades['trade_start'], self.trades['buy_price'], marker='^',s=70, color='green')
+        plt.scatter(self.trades['trade_end'], self.trades['sell_price'], marker='v',s=70, color='red')
+    
+        sns.despine()
+        plt.show()
+            
+            
+        
+        
+        
+        
+        
