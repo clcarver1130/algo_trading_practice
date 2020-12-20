96248ac335f11ce79e2d9f70d402e853500eb0a4
clcarver1130 <carver.cameron@gmail.com>
2020-12-20 16:02:08 -0600

    port set

--- bot_dashApp.py
+++ bot_dashApp.py
@@ -141,4 +141,4 @@ def load_layout():
 app.layout = load_layout
 
 if __name__ == '__main__':
-    app.run_server(debug=True, host='0.0.0.0')
+    app.run_server(debug=True, host='0.0.0.0', port=8050)
